8ef92ee49943425171f12be5eb45ec821d2d1768
Priyankaa Surisetty <159372550+PriSurisetty@users.noreply.github.com>
2024-08-11 12:10:48 -0500

    Update data.py

--- data.py
+++ data.py
@@ -12,12 +12,3 @@ response.raise_for_status()
 
 data = response.json()
 question_data = data["results"]
-
-
-"""What this module does:
-- Imports the requests module in order to request for api data (with url) and provides appropriate parameters
-- Saves the data into a variable called 'data'
-- Saves the question data into a variable called 'question_data' by tapping into the 'results' key which stores all
-of the information
-
-"""
