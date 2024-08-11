ff6a5a6224bf6d0644be55acf386fc2b4f06545b
Priyankaa Surisetty <159372550+PriSurisetty@users.noreply.github.com>
2024-08-11 12:10:59 -0500

    Update main.py

--- main.py
+++ main.py
@@ -17,14 +17,3 @@ quiz_ui = QuizInterface(quiz)
 
 print("You've completed the quiz")
 print(f"Your final score was: {quiz.score}/{quiz.question_number}")
-
-""" What this module does:
-- Initializes an empty list, and creates a for loop to iterate though all the data in question_data
-- Calls the 'Question' by creating a variable called 'new_question', passing in the parameters 'question_text'
-and 'question_answer' which were previously created by getting ahold of the keys 'correct_answer'
-and 'question'
-- We then appended the new_question for all the data (question and answers) into our question_bank
-- Then we finally pass 'new_question' as the q_list instance from the Quizbrain class and store this into
-an variable 'quiz'
-- 
-"""
